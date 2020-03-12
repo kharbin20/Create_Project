e623752c3cd33af267798db9b1ac318a9e73c067
Kennedi Harbin <kharbin20@bmchs.org>
2020-03-12 08:40:56 -0500

    Create project!

---
+++ Create_Project.py
@@ -0,0 +1,129 @@
+Name1 = input("Give me a boy name: ")
+Name2 = input("Give me another boy name: ")
+print ('''Despite it being a dark creepy night,
+you ''' + Name1 + ''' and ''' + Name2 + ''' decide to be outside.''')
+activity = input("Do you want to have a picnic or a go for a walk? ")
+while True:
+    if activity == "picnic":
+        print ('''you all sit down in a dark field and begin to eat. '''
+               + Name1 + ''' gets up to use the bathroom.''')
+        print ("you munch on a sandwhich while talking to " + Name2)
+        print ( Name1 + ''' has been gone a long time...
+            should we go check on him?''')
+        Decesion = input("are you going to go check on him? Yes or No? ")
+        if Decesion == "Yes" or "yes":
+            print('''
+            You and ''' + Name2 + ''' head to the bathroom area.
+            You hear a loud scream!
+            You sprint to the bathrooms.
+            You stoping seeing a pool of red coming from the stall.''')
+            print ("you open the door and " + Name2 + " screams")
+            print ("It's " + Name1 + "! And they've been decapitated!")
+            print("A gunshot sounds and " + Name2 + " falls limp.")
+            action = input("now would be a good time to run, Run? Or are you the crazy type who stays and fights?")
+            if action == "Run" or "run":
+                print('''
+                You take off sprinting, hope your a track star!
+                You get pretty far when you hear rustling in the bushes.
+                Suddenly a tall dark figure stands in front of you.
+                ''Hello there'' it says. You're definately not going back there.
+                Guess we have to converse. ''Hi'' you reply.
+                ''You look a little spooked, need help?''
+                ''')
+                trust = input("Do you trust him? Yes or No?")
+                if trust == "No" or "no":
+                    print('''it's silent. you can hear your heart beating.
+                            'Well than I'll be on my way.' he turns and leaves.
+                            You watch him walk off, ok?
+                            You stand there for a few seconds confused.
+                            You hear movement from behind you.
+                            You turn and barely dodge the knife hurled at you!
+                            RUN!!!!''')
+                    print('''
+                            You duck behind a tree, catching your breath
+                            Hopefully, whatever attacked you is gone.
+                            You ist and wait listening in silence.
+                            Silence....
+                            You poke your head around the tree.
+                            Seeing nothing, you walk out.
+                            The pathway is empty.
+                            Ok, so let's get back to the picnic.
+                            You walk back to the field.
+                            To your suprise, there's someone there.
+                            There's actually two people there!
+                            'Jeez ''' + Name2 + ''' how long does it take?'
+                            Your eyes widen.
+                            You stare back at ''' + Name1 + ''' and yourself on the blanket.
+                            HUH!?!?!?!?!?!
+                            ''')
+                    break
+                            
+                            
+    elif activity == "walk":
+        print('''
+                You and your friends walk down the dark spooky path.
+                You chit chat about random things.
+                Then you notice ''' + Name1 + ''' has stopped.
+                'What's wrong?' ''' + Name2 + ''' asks.
+                'I thought I heard something...' he says.
+                You peer into the dark bushes.
+                you jump back as a bird flies out.
+                ''')
+        print('''
+                You grumble as your friends laugh at you.
+                'You know crows are supposed to be evil' ''' + Name2 + ''' says.
+                'Oh no, ''' + Name1 + ''' jokes, 'you're cursed!'
+                You roll you eyes, 'yeah yeah, whatever.'
+                You yelp suddenly stepping into a hole.
+                Your friends keep you from completely falling.
+                You look down and see your foot is tangled in roots.
+                'Yo, what did you step in?' ''' + Name1 + ''' asks.
+                'How should I know?!' you reply.
+                They try to pull you out only for the vines to travel further up your legs.
+                'What the heck?' ''' + Name2 + ''' says, 'What's happening?'
+                'Let's cut it!' 'No let's see if we can untangle it.
+                ''')
+        answer = input("Do you Cut it or Untangle? ")
+        if answer == "Cut it" or "cut it":
+            print('''
+                    You watch ''' + Name2 + ''' pull out his pocket knife.
+                    He attempts to cut you free.
+                    he yelps as he's thrown back.
+                    You and ''' + Name1 + ''' gape.
+                    'What happened?' You ask.
+                    'I think I got shocked. I think I'm o-!'
+                    You and ''' + Name1 + ''' gape once again.
+                    A hole opened up in the ground and swallowed ''' + Name2 + '''!
+                    ''')
+            print('''
+                    'What just happened?!' ''' + Name1 + ''' asks.
+                    You wish you had an answer...
+                    'What should we d-!'
+                    You hit the dirt
+                    Your friend's support has left you.
+                    You look around and see ''' + Name + ''' is nowhere!
+                    Maybe you are cursed...
+                    You let out a yelp as the vines move up your legs.
+                    You claw at the dirt trying to gain traction.
+                    It's no use, you can't seem to gain any traction!
+                    You scream for help as the roots drag you under ground.
+                    You're not sure how much time passes before you awake.
+                    'Hey! Hey get up!'
+                    You look around and see fimilair faces.
+                    ''' + Name1 + ''' and ''' + Name2 + '''!
+                    'Where are we?' You ask.
+                    'Heck if we know!' ''' + Name2 + ''' replies.
+                    'Welcome to my humble abode.'
+                    You look around trying to find the new voice.
+                    You don't see anything and begin to wiggle.
+                    After a few seconds you hear a snap.
+                    Your friends laugh at you as you hit the dirt
+                    The  vines disperse and you stand.
+                    'Wiggle around! It'll break lose!'
+                    Your friends do so and join you.
+                    'so where are we
+                    
+                
+                            
+                
+
